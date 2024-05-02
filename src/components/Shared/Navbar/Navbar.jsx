7f9fb5bfce315eb804bc1b2405ba4d9fd724d135
yeasinbinali <yeasinbinali@gmail.com>
2024-05-02 10:51:33 +0600

    navbar added

--- src/components/Shared/Navbar/Navbar.jsx
+++ src/components/Shared/Navbar/Navbar.jsx
@@ -1,9 +1,37 @@
 import React from 'react';
+import { NavLink } from 'react-router-dom';
+import logo from '../../../../images/logo.png';
 
 const Navbar = () => {
+    const lists = [
+        <li style={{fontSize: '17px'}}><NavLink to='/'>Home</NavLink></li>,
+        <li style={{fontSize: '17px'}}><NavLink to='/about'>About</NavLink></li>,
+        <li style={{fontSize: '17px'}}><NavLink to='/register'>Register</NavLink></li>,
+        <li style={{fontSize: '17px'}}><NavLink to='/login'>Login</NavLink></li>,
+    ]
     return (
-        <div>
-            <h1>Navbar</h1>
+        <div style={{background: "whitesmoke"}}>
+            <div className="navbar w-[90%] mx-auto">
+                <div className="navbar-start">
+                    <div className="dropdown">
+                        <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
+                            <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
+                        </div>
+                        <ul tabIndex={0} className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52">
+                            {lists}
+                        </ul>
+                    </div>
+                    <img className='w-20 h-20 object-contain' src={logo} alt="" />
+                </div>
+                <div className="navbar-center hidden lg:flex">
+                    <ul className="menu menu-horizontal px-1">
+                        {lists}
+                    </ul>
+                </div>
+                <div className="navbar-end">
+                    <a className="btn">Appointment</a>
+                </div>
+            </div>
         </div>
     );
 };
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,13 +1,13 @@
-module.exports = {
-  content: ["./src/**/*.{html, js}"],
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
   theme: {
     extend: {},
   },
   plugins: [
     require('daisyui'),
-    {
-      tailwindcss: {},
-      autoprefixer: {},
-    },
   ],
-};
+}
