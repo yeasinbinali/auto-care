1c7cd6a82deca9f189e33018832e265b1183dc71
yeasinbinali <yeasinbinali@gmail.com>
2024-05-03 11:46:08 +0600

    register and login file added inside the authentication folder

---
+++ src/components/Authetication/Register/Register.jsx
@@ -0,0 +1,11 @@
+import React, { memo } from 'react';
+
+const Register = memo(() => {
+    return (
+        <div>
+            <h1>Register</h1>
+        </div>
+    );
+});
+
+export default Register;
